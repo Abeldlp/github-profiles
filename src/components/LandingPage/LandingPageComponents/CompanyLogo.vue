d496f8595b0fb40d906fe3a961e127de5262e034
Abel de la Paz <abel@foodticket.nl>
2021-12-01 20:17:47 +0100

    Implementation
    tailwindcss added to the project

--- src/components/LandingPage/LandingPageComponents/CompanyLogo.vue
+++ src/components/LandingPage/LandingPageComponents/CompanyLogo.vue
@@ -3,6 +3,7 @@
         <router-link to="/github-profiles">
             <h1>Logo</h1>
         </router-link>
+        <p class="border-2 border-red-500">This is a tailwind test</p>
     </div>
 </template>
 
